f34ed98ae5aff18855c899dd5374e617babedb9b
Ryan Darmayasa <111060495+layn25@users.noreply.github.com>
2025-10-02 19:02:10 +0800

    ketiga2

--- resources/views/alerts/sweetalert.blade.php
+++ resources/views/alerts/sweetalert.blade.php
@@ -78,39 +78,5 @@
             });
     }
 
-    const confirmTerima = (button) => {
-        const id = $(button).data('id');
-        Swal.fire({
-            title: "Apakah Anda Yakin?",
-            text: "Izin ini akan DITERIMA!",
-            icon: "question",
-            showCancelButton: true,
-            confirmButtonText: "Ya, Terima",
-            cancelButtonText: "Batal",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $('#terima-' + id).submit();
-            } else {
-                Swal.fire("Dibatalkan", "Izin tidak jadi diterima", "success");
-            }
-        });
-    };
-
-    const confirmTolak = (button) => {
-        const id = $(button).data('id');
-        Swal.fire({
-            title: "Apakah Anda Yakin?",
-            text: "Izin ini akan DITOLAK!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Ya, Tolak",
-            cancelButtonText: "Batal",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $('#tolak-' + id).submit();
-            } else {
-                Swal.fire("Dibatalkan", "Izin tidak jadi ditolak", "success");
-            }
-        });
-    };
+    
 </script>
